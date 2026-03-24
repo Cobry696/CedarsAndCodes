b15cf3e7165110dc8e4bb34bd4a1fc001d2f70e9
Colby <colbypynn@gmail.com>
2026-03-24 09:54:25 -0300

    initial commit

---
+++ gui.py
@@ -0,0 +1,27 @@
+from PySide6.QtWidgets import (
+    QApplication, 
+    QMainWindow, 
+    QPushButton,
+    QStatusBar,
+    QToolBar,
+    QWidget,
+    QTabWidget,
+    QGridLayout,
+    QLabel,
+    QMessageBox,
+    QComboBox,
+    QTextEdit,
+    QScrollArea
+)
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtGui import QAction, QIcon, QKeySequence, QFontDatabase, QFont, QTextCursor, QShortcut, QTextDocument
+from PySide6.QtWebEngineWidgets import QWebEngineView
+import sys
+import re
+
+app = QApplication(sys.argv)
+
+window = QMainWindow()
+window.show()
+
+app.exec()
