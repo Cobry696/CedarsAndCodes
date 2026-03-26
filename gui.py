1d82a85dd5869a8250476b5f5de75a2c2217c04b
Colby <colbypynn@gmail.com>
2026-03-26 09:41:37 -0300

    basic signin page with no functionality

--- gui.py
+++ gui.py
@@ -11,7 +11,9 @@ from PySide6.QtWidgets import (
     QMessageBox,
     QComboBox,
     QTextEdit,
-    QScrollArea
+    QScrollArea,
+    QVBoxLayout,
+    QLineEdit
 )
 
 # Hi
@@ -23,7 +25,43 @@ import re
 
 app = QApplication(sys.argv)
 
-window = QMainWindow()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Cedars&Codes")
+
+        self.setCentralWidget(LoginPage())
+
+class LoginPage(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(200,200,200,600)
+        self.setLayout(layout)
+
+        # text inputs for username and password
+        unameEntry = QLineEdit()
+        pwordEntry = QLineEdit()
+        pwordEntry.setEchoMode(QLineEdit.Password) # makes the password display as dots
+
+        # labels for page title and username/password entries
+        titleLabel = QLabel("Login/Sign Up")
+        unameLabel = QLabel("Username:")
+        pwordLabel = QLabel("Password:")
+
+        # actually adding the widgets to the layout
+        layout.addWidget(titleLabel)
+        layout.addWidget(unameLabel)
+        layout.addWidget(unameEntry)
+        layout.addWidget(pwordLabel)
+        layout.addWidget(pwordEntry)
+
+        
+
+        
+
+window = MainWindow()
 window.show()
 
 app.exec()
